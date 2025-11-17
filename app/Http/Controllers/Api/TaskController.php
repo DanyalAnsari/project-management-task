ab908f58d26b1d936b6eb9c5d2432410d3de6bd8
DanyalAnsari <mh.danyalansari@gmail.com>
2025-11-17 20:12:03 +0530

    implement task api

---
+++ app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\Project;
+use App\Models\Task;
+use App\Services\TaskService;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function __construct(private TaskService $service) {}
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        $data = $request->validated();
+        $project = Project::findOrFail($data['project_id']);
+        $user = auth()->user();
+        $this->authorize('create', [Task::class, $project]);
+        $task = $this->service->create($data, $project, $user);
+
+        return (new TaskResource($task->load('assignedUser')))
+            ->response()->setStatusCode(201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $data = $request->validated();
+        $user = auth()->user();
+        $task = $this->service->update($data, $task, $user);
+
+        return new TaskResource($task->load('assignedUser'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,22 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+
+    // auth
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
 
     Route::get('/projects', [ProjectController::class, 'index']);
 
+    // project 
+
     Route::post('/projects', [ProjectController::class, 'store']);
 
     Route::get('/projects/{project}', [ProjectController::class, 'show']);
+
+
+    // // Tasks
+    //  Route::post('/tasks', [TaskController::class, 'store']);         
+    // Route::put('/tasks/{task}', [TaskController::class, 'update']);   Employee
+    // Route::delete('/tasks/{task}', [TaskController::class, 'destroy']); 
 });
